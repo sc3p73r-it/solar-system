1f6ac7ba0fde5d99bae0e1aa7ae4045053198925
Htet Oo Wai Yan <140035139+sc3p73r-it@users.noreply.github.com>
2026-08-12 08:45:52 +0000

    update app.js

--- app.js
+++ app.js
@@ -13,16 +13,14 @@ app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("error!! " + err)
-    } else {
-       console.log("MongoDB Connection Successful")
-    }
+    pass: process.env.MONGO_PASSWORD
 })
+.then(() => {
+    console.log('MongoDB Connection Successful');
+})
+.catch((err) => {
+    console.error('MongoDB Connection Error:', err);
+});
 
 var Schema = mongoose.Schema;
 
